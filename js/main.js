7d98e44dd8fb0d7623e034843728495143717ab3
KarinaKovalchuk <114399164+KarinaKovalchuk@users.noreply.github.com>
2023-06-25 19:04:51 +0300

    edit main page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 window.onload = () => {
   loadMessages("received");
+  document.querySelector(".user__name").innerText = localStorage.getItem("user")
+}
+function loadCount() {
+  fetch(
+    `https://localhost:44396/api/Mail/get-count-unread-message?received=${localStorage.getItem("user")}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      document.getElementById("count-of-letters").innerText = ` ${data} `;
+    })
 }
 let menu = document.querySelectorAll(".menu img");
 menu.forEach((btn) => {
@@ -9,16 +20,18 @@ menu.forEach((btn) => {
     loadMessages(btn.id);
   });
 });
+
 function loadMessages(id) {
   console.log(id);
   let mainDiv = document.querySelector("#list-messages");
   mainDiv.innerHTML = "";
   fetch(
-    `https://localhost:44396/api/Mail/get-${id}-letters?${id}=examples@gmail.com`
+    `https://localhost:44396/api/Mail/get-${id}-letters?${id}=${localStorage.getItem("user")}`
   )
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
+
       data.forEach(item => {
         let div = document.createElement("div");
         div.className = "mail__object d-flex justify-content-between";
@@ -27,60 +40,84 @@ function loadMessages(id) {
         let isReaden = document.createElement("input");
         isReaden.type = "checkbox";
         isReaden.checked = item.isReaden;
-        isReaden.addEventListener("click", () => {
+        let trash = document.createElement("img");
+        trash.src = "../img/trash.png";
+        trash.addEventListener("click", () => {
           fetch(
-            `https://localhost:44396/api/Mail/set-is-readen?id=${item.id}`, {
+            `https://localhost:44396/api/Mail/delete-by-id?id=${item.id}`, {
             method: "POST",
           }).then((response) => {
             if (response.status == 200) {
-              loadMessages(document.querySelector("img.active-filter").id);
+              loadMessages();
             }
-            else {
-              alert(response.status);
-            }
-          }
-          )
-        })
-        if (item.isReaden) {
-          title.style.color = "lightGray"
-          isReaden.disabled = true;
-        }
-        div.append(title);
-        div.append(isReaden);
-        mainDiv.append(div);
-        title.addEventListener("click", () => {
-          // showPopup(item.title, item.desc);
-          document.getElementById("editModal").style.display = "block";
-          document.getElementById("editModal").classList.add("show");
-          document.getElementById("editModalTitle").innerText = item.title;
-          document.getElementById("modalDescription").innerText = item.description;
-          document.getElementById("btn-close").addEventListener("click", () => {
-            document.getElementById("editModal").style.display = "none";
-            document.getElementById("editModal").classList.remove("show");
+
           })
+          isReaden.addEventListener("click", () => {
+            fetch(
+              `https://localhost:44396/api/Mail/set-is-readen?id=${item.id}`, {
+              method: "POST",
+            }).then((response) => {
+              if (response.status == 200) {
+                loadMessages(document.querySelector("img.active-filter").id);
+              }
+              else {
+                alert(response.status);
+              }
+            }
+            )
+          })
+          if (item.isReaden) {
+            title.style.color = "lightGray"
+            isReaden.disabled = true;
+          }
+          div.append(title);
+          div.append(isReaden);
+          mainDiv.append(div);
+          title.addEventListener("click", () => {
+            // showPopup(item.title, item.desc);
+            document.getElementById("editModal").style.display = "block";
+            document.getElementById("editModal").classList.add("show");
+            document.getElementById("editModalTitle").innerText = item.title;
+            document.getElementById("sender-email").innerText = item.sender
+            document.getElementById("modalDescription").innerText = item.description;
+            document.getElementById("btn-close").addEventListener("click", () => {
+              document.getElementById("editModal").style.display = "none";
+              document.getElementById("editModal").classList.remove("show");
+            })
+          });
         });
+        if (data.lenght == 0) {
+          console.log(data)
+          mainDiv.innerHTML = "You don't have letters"
+        }
       });
-    });
+      loadCount();
+    }
+    )
 
 }
 document.querySelector("#btn-send").addEventListener("click", () => {
   let received = document.querySelector("#email").value;
   let title = document.getElementById("theme").value;
   let desc = document.querySelector("#text-message").value;
-  let sender = "examples@gmail.com";
+  let sender = localStorage.getItem("user");
 
   fetch(
     `https://localhost:44396/api/Mail/add-letter?title=${title}&desc=${desc}&sender=${sender}&received=${received}`,
     {
       method: "POST",
     })
-    .then((response) => {
-      if (response.status == 200) {
-        loadMessages(document.querySelector("img.active-filter").id);
-      }
-      else {
-        alert(response.status);
-      }
+    .then((response) => response.json())
+    .then((data) => {
+      fetch(
+        `https://localhost:44396/api/Mail/is-sended?id=${data.id}`
+      )
+        .then((response2) => response2.json())
+        .then((data2) => {
+          console.log(data2)
+          loadMessages(document.querySelector("img.active-filter").id);
+          alert(data2.status);
+        })
       clearFields();
     })
 })
